74605f8b2724deec85343055855a33e5adbf8bdf
HadIhab <chocovani101@hotmail.fr>
2021-04-23 18:05:05 +0100

    Passing the user back from google to session

--- app.js
+++ app.js
@@ -1,23 +1,48 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const passport = require('passport');
+const session = require('express-session');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+
+passport.use(new GoogleStrategy({
+	clientId: '',
+	clientSecret: '',
+	callbackURL: '',
+	function(req, accessToken, refreshToken, profile, done){
+		done(null, profile);
+	}
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, done) {
+  //done(null, user.id);
+  done(null, user);
+});
+passport.deserializeUser(function(user, done) {
+  //User.findById(id, function (err, user) {
+    done(err, user);
+  //});
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
